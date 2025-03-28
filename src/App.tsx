548a15e968e411a9445c6bc28f26c0d358ee1099
web-ma-006 <jinyuma007+github@gmail.com>
2025-03-28 10:51:26 +0800

    添加GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme';
@@ -11,12 +11,31 @@ import YamlPage from './pages/YamlPage';
 import EntityPage from './pages/EntityPage';
 import ExcelPage from './pages/ExcelPage';
 import EditorPage from './pages/EditorPage';
+import { useEffect } from 'react';
+
+// 添加路由变化追踪组件
+function RouteTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // 当路由变化时，发送页面访问事件
+    if (typeof window.gtag !== 'undefined') {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname,
+        page_title: document.title
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
+        <RouteTracker />
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<FormatPage />} />
